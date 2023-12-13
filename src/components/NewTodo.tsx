fb13d7f6253a02aef306e75787b3f55d2d2869c2
unknown <heroglasses155@gmail.com>
2023-12-13 11:12:33 +0630

    usage of useRef

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,21 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
   const handleOnSubmit = (event: React.FormEvent) => {
     event.preventDefault();
+
+    const enteredText = inputRef.current!.value;
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
   };
 
   return (
     <form onSubmit={handleOnSubmit}>
       <label htmlFor="text">Todo Text</label>
-      <input type="text" id="text" />
+      <input type="text" id="text" ref={inputRef} />
       <button>Submit</button>
     </form>
   );
